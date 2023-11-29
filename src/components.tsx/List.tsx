395b7d360c636390a09b123a300f4dd5f52ca3c5
Maria Luisa <211039617@aluno.unb.br>
2023-11-29 16:10:58 -0300

    feat: implementa delete

--- src/components.tsx/List.tsx
+++ src/components.tsx/List.tsx
@@ -1,6 +1,32 @@
 import { Circle, Trash } from "@phosphor-icons/react";
+import { useState } from 'react'
+
 import styles from './List.module.css';
+
 export function List() {
+    const  [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: 'Fazer café',
+            done: false
+        },
+        {
+            id: 2,
+            title: 'Lavar a louça',
+            done: false
+        }
+    ])
+
+    function handleRemoveTodo(commentIdToDelete: number) {
+        const todoListWitouthDeleteOne = todos.filter(todo => {
+            return todo.id !== commentIdToDelete 
+        })
+        setTodos(todoListWitouthDeleteOne)
+    }
+
+
+
+
     return(
         <div className={styles.list}>
             <div className={styles.listInfo}>
@@ -14,45 +40,24 @@ export function List() {
                 </div>
             </div>
 
+            
+                {todos.map(todo => {
 
+                    return (
+                        <div className={styles.boxActivit}>
+                            <div className={styles.activit}>
+                                <button className={styles.circle}>
+                                    <Circle/>
+                                </button>
+                                <p>{todo.title}</p>
+                            </div>
+                            <button onClick={() => handleRemoveTodo(todo.id)} className={styles.trash}>
+                                <Trash />
+                            </button>
+                        </div>
 
-
-
-            <div className={styles.boxActivit}>
-                <div className={styles.activit}>
-                    <button className={styles.circle}>
-                        <Circle/>
-                    </button>
-                    <p>Tenho que lavar a louça</p>
-                </div>
-                <button className={styles.trash}>
-                    <Trash/>
-                </button>
-            </div>
-
-            <div className={styles.boxActivit}>
-                <div className={styles.activit}>
-                    <button className={styles.circle}>
-                        <Circle/>
-                    </button>
-                    <p>Tenho que lavar a louça</p>
-                </div>
-                <button className={styles.trash}>
-                    <Trash/>
-                </button>
-            </div>
-
-            <div className={styles.boxActivit}>
-                <div className={styles.activit}>
-                    <button className={styles.circle}>
-                        <Circle/>
-                    </button>
-                    <p>Tenho que lavar a louça</p>
-                </div>
-                <button className={styles.trash}>
-                    <Trash/>
-                </button>
-            </div>
+                    )
+                })}
         
         </div>
     )
